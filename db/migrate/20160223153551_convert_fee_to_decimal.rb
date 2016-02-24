3f3e1078c9b6fa8842c1229da76f871384e62553
marcjennings <marc.jennings31@gmail>
2016-02-24 16:40:24 +0000

    changed convert fee migration

--- db/migrate/20160223153551_convert_fee_to_decimal.rb
+++ db/migrate/20160223153551_convert_fee_to_decimal.rb
@@ -5,6 +5,9 @@ class ConvertFeeToDecimal < ActiveRecord::Migration
   		p.save!
   	end
 
-  	change_column :products, :fee, :decimal, default: 0.0
+  	if Rails.env.production?
+  		change_column :products, :fee, 'numeric(10,2) USING CAST(fee as numeric(10,2))'
+  	else
+      change_column :products, :fee, :decimal, default: 0.0
   end
 end
